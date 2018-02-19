ace3f892533985e4d430d681c84f017d466ab689
Yanela Pachacama <yanela.pachacama@tecsup.edu.pe>
2018-02-19 12:47:37 -0500

    Proyecto MAVEN

---
+++ src/main/java/PcOMPARTIDO/App.java
@@ -0,0 +1,13 @@
+package PcOMPARTIDO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
